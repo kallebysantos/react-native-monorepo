a5bf903d9b87e8b95c11d52fe04b71b377812ddd
kallebysantos <kalleby_santos@hotmail.com>
2023-07-27 17:22:59 +0100

    📦 NEW: Publishing and hosting web client from .NET

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapFallbackToFile("~/index.html");
 
 app.Run();
